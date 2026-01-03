89b03698192540fc570ab6dc945d4e1a2acb8e7a
Prince1304 <kyadaprince43@gmail.com>
2026-01-03 17:41:27 +0530

    Add files via upload

---
+++ Assignment-1_task-1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter a First Number:"))
+num2=int(input("Enter a Second Number:"))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print("Addition Is:",add)
+print("Subtraction Is:",sub)
+print("Multiplication Is:",mul)
+print("Division Is:",div)
---
+++ Assignment-1_task-2.py
@@ -0,0 +1,4 @@
+f_name = input("Enter Your First name: ")
+l_name = input("Enter Your Last name: ")
+Full_name = f_name + " " + l_name
+print("Hello, " + Full_name +" I Happy To Joining Team")
