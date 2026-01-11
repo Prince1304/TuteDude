294dea93b711ec1c9f1a718a13dce8e7b19afae1
Prince1304 <kyadaprince43@gmail.com>
2026-01-11 13:32:23 +0530

    Add files via upload

---
+++ Assignment-3_Task-1.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+n = int(input("Enter a number: "))
+answer = int(factorial(n))
+print(f"The Factorial Of {n} is {answer}")
---
+++ Assignment-3_Task-2.py
@@ -0,0 +1,12 @@
+import math
+
+num = int(input("Enter a number: "))
+
+Square = math.sqrt(num)
+Natural = math.log(num)
+Sine = math.sin(Natural)
+
+print("===Answer===")
+print("Square Root: ", Square)
+print("Natural : ", Natural)
+print("Sine : ", Sine)
